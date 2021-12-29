a98b208b4329ad5bae43658059bd069bb2c43c94
Furkan Samaraz <92402372+FurkanSamaraz@users.noreply.github.com>
2021-12-29 17:20:48 +0300

    Add files via upload

---
+++ opencv.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/koyachi/go-nude"
+)
+
+func main() {
+	resimler := []string{
+		"heykel.jpg", "lena.gif", "çift.png", "koşucu.png"}
+	for _, resim := range resimler {
+		ciplakmi, _ := nude.IsNude(resim)
+		fmt.Println(ciplakmi, "\t", resim)
+	}
+
+}
